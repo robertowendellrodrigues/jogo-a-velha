c444495c8a980a7f08bfb5e6a987eccb2403f1a1
WendellRodrigues <robertowendellrodrigues@gmail.com>
2021-11-15 21:43:57 -0300

    Agora sim

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const jogador1 = 1
 const jogador2 = 2
 let vez = jogador1
-var jogada = [0,0,0,0,0,0,0,0,0]
+var jogada = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 let fim = false
-var jogadas = 10
+var jogadas = 9
 
-function jogar(n) {  
+function jogar(n) {
 
     if (jogada[n] == 0 && fim == false) {
         jogada[n] = vez
+        jogadas -= 1
         let img = document.querySelector(`#img${n}`)
         img.setAttribute('src', `img/jogada${vez}.png`)
         if (vez == jogador1) {
@@ -19,54 +20,54 @@ function jogar(n) {
         }
     }
     quemGanhou()
-    
+
 }
 
 function quemGanhou() {
     fim = false
     let a, b, c
-    jogadas -= 1
-   
-    if (jogada[0] == jogada[1] && jogada[1] == jogada[2] && jogada[0] != 0) {
+
+
+    if (jogada[0] === jogada[1] && jogada[1] === jogada[2] && jogada[1] !== 0) {
         a = [jogada[0], 0], b = [jogada[1], 1], c = [jogada[2], 2]
         fim = true
     }
-    if (jogada[0] == jogada[4] && jogada[4] == jogada[8] && jogada[0] != 0) {
+    if (jogada[0] === jogada[4] && jogada[4] === jogada[8] && jogada[4] !== 0) {
         a = [jogada[0], 0], b = [jogada[4], 4], c = [jogada[8], 8]
         fim = true
     }
-    if (jogada[0] == jogada[3] && jogada[3] == jogada[6] && jogada[0] != 0) {
+    if (jogada[0] === jogada[3] && jogada[3] === jogada[6] && jogada[3] !== 0) {
         a = [jogada[0], 0], b = [jogada[3], 3], c = [jogada[6], 6]
         fim = true
     }
-    if (jogada[2] == jogada[5] && jogada[5] == jogada[8] && jogada[2] != 0) {
+    if (jogada[2] === jogada[5] && jogada[5] === jogada[8] && jogada[2] !== 0) {
         a = [jogada[2], 2], b = [jogada[5], 5], c = [jogada[8], 8]
         fim = true
     }
-    if (jogada[2] == jogada[4] && jogada[4] == jogada[6] && jogada[2] != 0) {
+    if (jogada[2] === jogada[4] && jogada[4] === jogada[6] && jogada[2] !== 0) {
         a = [jogada[2], 2], b = [jogada[4], 4], c = [jogada[6], 6]
         fim = true
     }
-    if (jogada[1] == jogada[4] && jogada[4] == jogada[7] && jogada[1] != 0) {
+    if (jogada[1] === jogada[4] && jogada[4] === jogada[7] && jogada[1] !== 0) {
         a = [jogada[1], 1], b = [jogada[4], 4], c = [jogada[7], 7]
         fim = true
     }
-    if (jogada[6] == jogada[7] && jogada[7] == jogada[8] && jogada[6] != 0) {
+    if (jogada[6] === jogada[7] && jogada[7] === jogada[8] && jogada[6] !== 0) {
         a = [jogada[6], 6], b = [jogada[7], 7], c = [jogada[8], 8]
         fim = true
     }
-    if (jogada[3] == jogada[4] && jogada[4] == jogada[5] && jogada[3] != 0) {
+    if (jogada[3] === jogada[4] && jogada[4] === jogada[5] && jogada[3] !== 0) {
         a = [jogada[3], 3], b = [jogada[4], 4], c = [jogada[5], 5]
         fim = true
     }
     if (jogadas <= 0) {
         fim = true
-        jogadas = 10
+        jogadas = 9
     }
 
     document.querySelector(`#img${a[1]}`).setAttribute('src', `img/ajogada${a[0]}.png`)
     document.querySelector(`#img${b[1]}`).setAttribute('src', `img/ajogada${b[0]}.png`)
-    document.querySelector(`#img${c[1]}`).setAttribute('src', `img/ajogada${c[0]}.png`)    
+    document.querySelector(`#img${c[1]}`).setAttribute('src', `img/ajogada${c[0]}.png`)
 }
 
 function reiniciaJogo() {
